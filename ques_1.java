4ce9f4e91ae506f3ecba76823e136405d12a5c34
Shivang Gupta <iamshivang25@gmail.com>
2026-02-04 12:53:59 +0530

    Add files via upload

---
+++ ques_1.java
@@ -0,0 +1,6 @@
+public class ques_1 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ ques_3.java
@@ -0,0 +1,14 @@
+public class ques_3 {
+    public static void main(String[] args) {
+
+        String[] arr = args;
+
+        System.out.println("Array elements are:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
+
