c75bbdfdca89537b9697f3d49bd0b2987913eb0d
Shivang Gupta <iamshivang25@gmail.com>
2026-02-04 12:54:21 +0530

    Add files via upload

---
+++ ques_5.java
@@ -0,0 +1,13 @@
+public class ques_5 {
+
+    int number = 100;
+
+    public static void main(String[] args) {
+
+        ques_5 obj = new ques_5();
+
+        System.out.println("Value = " + obj.number);
+    }
+}
+
+
