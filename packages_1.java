b85b45a6790e76d9ed9a3a82fc46fcb83e4f12e3
Shivang Gupta <iamshivang25@gmail.com>
2026-02-12 08:54:34 +0530

    Use of package

---
+++ packages_1.java
@@ -0,0 +1,17 @@
+package package1;
+
+class Car{
+    int milage;
+    int price;
+}
+ public class packages_1 {
+ 
+    public static void main(String[] args) {
+        Car c= new Car();
+        c.milage= 100;
+        c.price= 2345667;
+
+    }
+ }
+
+ 
